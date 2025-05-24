135600f7f5971ae02917ca51b16d62a09e605cc2
sarahvue <sarahvue0924@gmail.com>
2025-05-24 14:49:10 -0700

    Added functionality

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
+require("datejs");
+const username1 = ["sodaGirl1", "Poppy555"];
+const username2 = ["helloWorld26", "codingpal09"];
+const username3 = ["imBatman2", "flash99"];
 
+function combineUsers(...usernames) {
+  const combinedObject = {
+    users: [],
+  };
 
+  for (let user of usernames) {
+    combinedObject.users = [...combinedObject.users, ...user];
+  }
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+const allUsers = combineUsers(username1, username2, username3);
+console.log(allUsers);
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
